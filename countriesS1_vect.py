9e3c15f5f6419f9d52db9028aa8785c1cd27ae67
Aldo Pareja <aldo.pareja@ibm.com>
2018-05-13 18:24:04 -0400

    fixed some bugs and increased accuracy, will start iterating over the targets now, rather than the consequences

--- countriesS1_vect.py
+++ countriesS1_vect.py
@@ -2,8 +2,8 @@ import numpy as np
 import torch
 from torch.autograd import Variable
 import torch.nn.functional as F
-import datetime
 from pudb import set_trace;
+import time
 
 with open('data/countries_s1') as f:
     facts = f.read().splitlines()
@@ -106,7 +106,7 @@ def leaveTopK(preds,K):
     _,idx = torch.sort(preds[:,-1],descending=True)
     preds = preds[idx,:]
     out = preds[0,:].unsqueeze(0)
-    for i in range(1,K):
+    for i in range(1,min(K,preds.size()[0])):
         t = preds[i,:].unsqueeze(0)
         if t[:,-1] == 0:
             break
@@ -124,6 +124,7 @@ def forward_step(facts,K):
     num_facts = facts.size()[0]
     #rule 1
     # b1(x,y)<-b2(y,x)
+    start_time = time.time()
     rule_expanded = rules[0].repeat(num_facts,1)
     preds_r1 = F.cosine_similarity(rule_expanded[:,num_predicates:],facts[:,:num_predicates],dim=1)
     preds_r1 = preds_r1*facts[:,-1]
@@ -133,8 +134,8 @@ def forward_step(facts,K):
                          facts[:,num_predicates:num_predicates+num_constants],
                          preds_r1),dim=1)
     # print(preds_r1)
-
     preds_r1 = leaveTopK(preds_r1,K)
+    
     #rule 2
     #b1(x,y)<-b2(x,z),b3(z,y)
     body1 = facts.repeat((1,num_facts)).view(-1,num_feats_per_fact+1)
@@ -142,13 +143,21 @@ def forward_step(facts,K):
     rule_expanded = rules[1].repeat(body1.size()[0],1)
     #previous scores
     preds_r2 = body1[:,-1]*body2[:,-1]
+    #similarity between shared constants
+    preds_r2 = preds_r2*F.cosine_similarity(body1[:,num_predicates+num_constants:-1],
+                                            body2[:,num_predicates:num_predicates+num_constants],dim=1)
+    #remove 0 scores to improve speed - if constant isn't shared we don't compute anything else
+    non_zero = preds_r2.nonzero().squeeze()
+    preds_r2 = preds_r2[non_zero]
+    rule_expanded = rule_expanded[non_zero,:]
+    body1 = body1[non_zero,:]
+    body2 = body2[non_zero,:]
+    
     #predicate of body1 with predicate of rule
     preds_r2 = preds_r2*F.cosine_similarity(rule_expanded[:,num_predicates:2*num_predicates],body1[:,:num_predicates],dim=1)
     #predicate of body2 with predicate of rule
     preds_r2 = preds_r2*F.cosine_similarity(rule_expanded[:,2*num_predicates:],body2[:,:num_predicates],dim=1)
-    #similarity between shared constants
-    preds_r2 = preds_r2*F.cosine_similarity(body1[:,num_predicates+num_constants:-1],
-                                            body2[:,num_predicates:num_predicates+num_constants],dim=1)
+    
     preds_r2 = preds_r2.unsqueeze(1)
     preds_r2 = torch.cat((rule_expanded[:,:num_predicates]
                          ,body1[:,num_predicates:num_predicates+num_constants]
@@ -158,6 +167,7 @@ def forward_step(facts,K):
     #removing repeated facts and leaving ones with highest score
     preds_r2 = leaveTopK(preds_r2,K)
     out = torch.cat((preds_r1,preds_r2),dim=0)
+    print("fws took %s" % (time.time() - start_time))
     return out
     # return preds_r2
 
@@ -171,7 +181,7 @@ def forward_step(facts,K):
 target = Variable(knowledge_pos)
 no_samples = 50
 
-num_iters = 200
+num_iters = 100
 learning_rate = .1
 lamb = 1
 
@@ -187,9 +197,11 @@ K = 30 ##For top K
 #consequences: facts to be looked for maximum similarities across target
 #target: set of facts that are assumed to be true
 #testing: returns the probabilities of matched facts, a prediction is considered true if p>0.5
-def find_max_similarities(consequences,target,testing=False):    
+def find_max_similarities(consequences,target,testing=False):
+    start_time = time.time()    
     num_consequences = consequences.size()[0]
     num_targets = target.size()[0]
+
     #each consequence repeated by the number of targets
     if testing:
         #for each target find max similarity across consequences
@@ -199,17 +211,30 @@ def find_max_similarities(consequences,target,testing=False):
         #for each consequence compute the similarity with all targets
         tmp_c = consequences.repeat(1,num_targets).view(-1,num_feats_per_fact+1)
         tmp_t = target.repeat(num_consequences,1)
-    sim = F.cosine_similarity(tmp_c[:,:num_predicates],tmp_t[:,:num_predicates],dim=1)
-    sim = sim * F.cosine_similarity(tmp_c[:,num_predicates:num_predicates+num_constants],
-                                    tmp_t[:,num_predicates:num_predicates+num_constants],dim=1)
-    sim = sim * F.cosine_similarity(tmp_c[:,num_predicates+num_constants:-1],
-                                    tmp_t[:,num_predicates+num_constants:],dim=1)
+
+    sim = F.cosine_similarity(tmp_c[:,num_predicates:num_predicates+num_constants],
+                              tmp_t[:,num_predicates:num_predicates+num_constants],dim=1)
+    #only compute for non-zero values to speed up
+    non_zero = sim.nonzero().squeeze()
+    #if no unification return 0
+    if non_zero.size()[0]==0:
+        sim = torch.zeros_like(sim)
+    else:
+        sim[non_zero] = sim[non_zero] * F.cosine_similarity(tmp_c[non_zero,num_predicates+num_constants:-1],tmp_t[non_zero,num_predicates+num_constants:],dim=1)
+
+    non_zero = sim.nonzero().squeeze()
+    if non_zero.size()[0]==0:
+        sim = torch.zeros_like(sim)
+    else:
+        sim[non_zero] = sim[non_zero] * F.cosine_similarity(tmp_c[non_zero,:num_predicates] ,tmp_t[non_zero,:num_predicates],dim=1)
+    
     #for each consequence/target, get the maximum simlarity with the set of targets/consequences
     if testing:
         sim = sim.view(-1,num_consequences)
     else:
         sim = sim.view(-1,num_targets)
     m, idx = torch.max(sim,dim=1)
+    print("fms took %s" % (time.time() - start_time))
     if testing:
         return m, tmp_c[idx,-1]
     return m
@@ -287,8 +312,8 @@ with open('test_acc_s1_neigh_sample','w') as f:
 
         #computing test results
         print('computing test results')
-        K_tmp = 200
-        facts = torch.cat((knowledge_pos, Variable(torch.ones(core_rel.size()[0], 1))), 1)
+        K_tmp = 300
+        facts = torch.cat((knowledge_pos, Variable(torch.ones(knowledge_pos.size()[0], 1))), 1)
         consequences = forward_step(facts,K_tmp)
         for step in range(1,steps):
             tmp = torch.cat((consequences,facts),dim=0)
